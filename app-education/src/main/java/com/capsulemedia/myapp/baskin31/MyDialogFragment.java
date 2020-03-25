0bcd99c08ca339802b376a415a04220c94a4d561
penpen-dongE <dongheepark.dev@gmail.com>
2020-03-25 19:49:38 +0900

    baskin31 package modification

---
+++ app-education/src/main/java/com/capsulemedia/myapp/baskin31/MyDialogFragment.java
@@ -0,0 +1,6 @@
+package com.capsulemedia.myapp.baskin31;
+
+import android.support.v4.app.DialogFragment;
+
+public class MyDialogFragment extends DialogFragment {
+}
---
+++ app-education/src/main/java/com/capsulemedia/myapp/baskin31/VariableScrollSpeed.java
@@ -0,0 +1,6 @@
+package com.capsulemedia.myapp.baskin31;
+
+import android.support.v7.widget.LinearLayoutManager;
+
+public class VariableScrollSpeed extends LinearLayoutManager {
+}
